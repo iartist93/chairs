4861cb58a727810e3290c047969de30212e1a204
ahmad <a.ayman@uniparticle.com>
2021-10-18 14:19:33 +0200

    feat: split the page into 2 sides

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,14 @@
           <div class="avatar_wrapper">
             <img class="avatar" src="~/assets/avatar.jpg" alt="avatar" />
           </div>
+          <div class="account_wrapper">
+            <div class="name">Ahmad</div>
+            <div class="logout">
+              <button class="logout_button">
+                logout
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <div class="content">
@@ -29,9 +37,58 @@ export default {};
 </script>
 
 <style lang="scss">
-.avatar_wrapper {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
+*,
+html,
+div,
+p,
+span {
+  padding: 0;
+  margin: 0;
+}
+
+.container {
+  display: flex;
+}
+
+.side {
+  height: 100vh;
+}
+
+.left {
+  width: 54.99%;
+  padding: 20px;
+  display: flex;
+}
+
+.right {
+  width: 44.99%;
+  background-color: #f0f7f7;
+  padding: 20px;
+}
+
+.account {
+  display: flex;
+
+  .name {
+    font-size: 25px;
+  }
+
+  .avatar_wrapper {
+    margin-right: 20px;
+
+    .avatar {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
+  }
+}
+
+.logout {
+  .logout_button {
+    outline: none;
+    border: none;
+    background-color: transparent;
+  }
 }
 </style>
